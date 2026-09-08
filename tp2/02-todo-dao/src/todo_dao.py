fbb715d9269bc1a6d1ee23df16af0660da50d006
eolem <100517246+eolem@users.noreply.github.com>
2026-09-08 14:30:34 +0200

    fix tp2

--- tp2/02-todo-dao/src/todo_dao.py
+++ tp2/02-todo-dao/src/todo_dao.py
@@ -2,17 +2,14 @@ from todo import Todo
 import sqlite3
 from pprint import pprint
 
-from deco import do_log
+
 class TodoDAO:
-    
 
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self._con = sqlite3.connect(db_file)
 
-    def __init__(self,db_file):
-       self.db_file = db_file
-       self._con = sqlite3.connect(db_file)
-
-    
-    def save(self,todo:Todo):
+    def save(self, todo: Todo):
         cur = self._con.cursor()
         cur.execute(f"""
             INSERT INTO todos_tbl (title,completed) 
@@ -21,7 +18,6 @@ class TodoDAO:
         """)
         self._con.commit()
 
-    @do_log('meslogs.log')    
     def find_all(self):
         """
         find_all c'est bien
@@ -31,7 +27,7 @@ class TodoDAO:
         res = cur.execute("SELECT id,title,completed FROM todos_tbl")
         todos = res.fetchall()
         for t in todos:
-            todo = Todo(*t)#Todo(id=t[0],title=t[1],completed=t[2]) 
+            todo = Todo(*t)  # Todo(id=t[0],title=t[1],completed=t[2])
             yield todo
         #     all.append(todo)
         # return all
