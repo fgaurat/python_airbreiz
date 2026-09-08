8e2a59b8f032edcfcd88eca7f1168fb01939ea02
eolem <100517246+eolem@users.noreply.github.com>
2026-09-08 12:30:20 +0200

    add tp 01

---
+++ tp2/01-test-geo/tests/test_rectangle.py
@@ -0,0 +1,62 @@
+from rectangle import Rectangle
+import pytest
+
+
+def test_surface():
+    # Arrange
+    r = Rectangle(2, 3)
+    # Act
+    surface = r.surface
+    # Assert
+    assert surface == 6
+
+
+@pytest.mark.parametrize(
+    ("longueur, largeur, surface_attendue"),
+    [
+        pytest.param(2, 3, 6, id="valeurs entières"),
+        pytest.param(2, 0, 0, id="largeur nulle"),
+    ]
+)
+def test_surface_with_params(longueur, largeur, surface_attendue):
+    # Arrange
+    r = Rectangle(longueur, largeur)
+    # Act
+    surface = r.surface
+    # Assert
+    assert surface == surface_attendue
+
+
+def test_surface_type_int():
+    # Arrange
+    r = Rectangle(2, 3)
+    # Act
+    surface = r.surface
+    # Assert
+    assert isinstance(surface, int)
+
+
+def test_comparaison_egaux():
+    # Arrange
+    r1 = Rectangle(2, 3)
+    r2 = Rectangle(2, 3)
+
+    # Act
+    c = r1 == r2
+
+    # Assert
+    assert c
+    #     assert Rectangle(2, 3) == Rectangle(2, 3)
+
+
+def test_comparaison_pas_egaux():
+    # Arrange
+    r1 = Rectangle(2, 3)
+    r2 = Rectangle(2, 4)
+
+    # Act
+    c = r1 != r2
+
+    # Assert
+    assert c
+    #     assert Rectangle(2, 3) != Rectangle(2, 3)
